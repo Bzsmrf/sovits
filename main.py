20dd78aede14ed34a99e4b154d9a8111e0d5fae1
Bzsmrf <dozerzzzz1234@gmail.com>
2023-05-10 20:33:19 +0530

    update

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ REPO_URL = "https://github.com/Bzsmrf/so-vits-svc-fork.git"
 # Branch (for development)
 if not os.path.exists('so-vits-svc-fork'):
     os.system(f'git clone {REPO_URL}')
+if not os.path.exists('input'):
+    os.mkdir('input')
+if not os.path.exists('output'):
+    os.mkdir('output')
+
 
 os.chdir('so-vits-svc-fork\src\so_vits_svc_fork\inference')
 
-print(os.getcwd())
+subprocess.run(['pip', 'install', 'so_vits_svc_fork'])
 
 subprocess.run(['python', 'inference.py'])
