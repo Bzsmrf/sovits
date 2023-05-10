6c9edd803ed24c8b5178d89fec5001fe33ba6ec4
Bzsmrf <dozerzzzz1234@gmail.com>
2023-05-10 22:31:11 +0530

    update

--- so-vits-svc-fork/src/so_vits_svc_fork/inference/inference.py
+++ so-vits-svc-fork/src/so_vits_svc_fork/inference/inference.py
@@ -2,15 +2,15 @@ from main import *
 
 
 artistDictionary = {}
-artistDictionary['Drake']  = "G_166400.pth config.json 0"
-artistDictionary['Eminem'] = "G_86400.pth  config.json 0"
-artistDictionary['Kanye']  = "G_199200.pth config.json 0"
+artistDictionary['Drake']  = "so-vits-svc-fork\src\so_vits_svc_fork\inference\drake05 G_166400.pth config.json 0"
+artistDictionary['Eminem'] = "so-vits-svc-fork\src\so_vits_svc_fork\inference\eminem general model 86k.zip G_86400.pth  config.json 0"
+artistDictionary['Kanye']  = "so-vits-svc-fork\src\so_vits_svc_fork\inference\ye200k G_199200.pth config.json 0"
 print("Artist List: Drake, Eminem, Kanye.")
 name = input("Enter Artist: ")
-def convertAudio(artistDictioary, name):
+def convert_with_sovits(artistDictioary, name):
     if name not in artistDictioary:
         print("Invalid Artist")
         return
     myList = artistDictioary[name].split()
-    infer(input_path='input/input.wav', output_path= 'output/output.wav', model_path = myList[0], config_path = myList[1], speaker = myList[2])
-convertAudio(artistDictionary, name)
+    infer(input_path='input/input.wav', output_path= 'output/output.wav', model_path = ' '.join(myList[0:-3]) + "/" + myList[-3], config_path = ' '.join(myList[0:-3]) + "/" + myList[-2], speaker = myList[-1])
+convert_with_sovits(artistDictionary, name)
